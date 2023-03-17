201560040ee0017c685746b01866de1eae57d2ef
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2023-03-17 17:40:48 +0200

    write link manager

--- extra_class/LinksManager.py
+++ extra_class/LinksManager.py
@@ -1,6 +1,41 @@
+import os.path as path
+import os, sys
+
+models_directory = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/models/')
+sys.path.append(models_directory)
+
+from catalog import Category
+from pandas import DataFrame
+
 class LinkManager:
-    __path = ""
+    __path : str
+    DataCategory : Category
     
     def __init__(self, _path: str):
         self.__path = _path
+        self.DataCategory = Category(_path)
         
+
+    def go_to_next_catalog(self, next_directive):
+        self.DataCategory.catalog_path = path.join(self.DataCategory.catalog_path,next_directive)
+        self.DataCategory.read_json_catalog()
+        
+    def back_to_prev_catalog(self):
+        dir_list = self.DataCategory.catalog_path.split("\\")
+        self.DataCategory.catalog_path =  "\\".join(  [ dir_list[dir_index_item]  for dir_index_item in range( 0, len( dir_list ) - 1 )  ] )
+    
+    def get_all_subdirectory(self):
+        subdirectives = []
+        for index, row in self.DataCategory.data.iterrows():
+            if "is_catalog" in self.DataCategory.data.columns: 
+                if row["is_catalog"] == True:
+                    if "name" in self.DataCategory.data.columns: 
+                        subdirectives.append(row["name"])
+        print(subdirectives)
+        
+        
+                            
+linkmanager =LinkManager("D:\\GitHub\\educationTelegramBot\\data");
+linkmanager.get_all_subdirectory()
+linkmanager.go_to_next_catalog("start")
+linkmanager.get_all_subdirectory()
---
+++ models/catalog.py
@@ -0,0 +1,20 @@
+from typing import List
+from pprint import pprint
+import os.path as path
+from pandas import DataFrame, read_json
+
+class Category:
+    json_hash: str
+    catalog_path: str
+    data: DataFrame
+    
+
+    def __init__(self, path: str):
+        self.catalog_path = path
+        self.read_json_catalog();
+        
+    
+    def read_json_catalog(self):
+        self.data = read_json(path.join(self.catalog_path , "request_router.json") ,encoding='cp1251', orient="records ")
+    
+        
